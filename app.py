a7e08d71093ec586d9158aa2726f44ed91faa738
Rammus <rammus@fever38.com>
2019-03-01 18:46:01 +0800

    Implement image upload/download

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, redirect, url_for, request, jsonify, Response, send_file
-from pymongo import MongoClient
+from flask import Flask, redirect, url_for, request, jsonify, Response, send_file, send_from_directory
+from pymongo import MongoClient, ReturnDocument
 from bson.json_util import dumps
 import json
 import uuid
@@ -20,6 +20,7 @@ def show():
 
     for doc in db.photos.find():
         doc.pop('_id') 
+        doc['url'] = f"{request.host}/img/{doc['uuid']}"
         photos.append(doc)
         
     return jsonify(photos),200
@@ -30,54 +31,87 @@ def show():
 @app.route('/photos/<photo_id>', methods=['GET'])
 def get(photo_id):
     doc = db.photos.find_one({"uuid":photo_id})
-    doc.pop('_id')
 
-    return jsonify(doc), 200
+    if doc is None:
+        return jsonify({}), 404
+    else:
+        doc.pop('_id')
+        doc['url'] = f"{request.host}/img/{doc['uuid']}"
+        return jsonify(doc), 200
 
 
 @app.route('/photos/<photo_id>', methods=['PATCH'])
 def update(photo_id):
 
     data = request.get_json()
-    name = data['name']
-    description = data['description']
+    
+    changes = {}
+    if 'name' in data:
+        changes['name'] = data['name'] 
+    if 'description' in data:
+        changes['description'] = data['description']
 
-    doc = db.photos.find_one({"uuid":photo_id})
-    doc.pop('_id')
-    if name:
-        doc['name'] = description
+    doc = db.photos.find_one_and_update(
+        {"uuid":photo_id},
+        { "$set": changes ,},
+        return_document=ReturnDocument.AFTER
+    )
 
-    if description:
-        doc['description'] = description
-
-    doc.save()
-    return jsonify(doc), 200
+    if doc is None:
+        return jsonify({}), 404
+    else:
+        doc.pop('_id')
+        doc['url'] = f"{request.host}/img/{doc['uuid']}"
+        return jsonify(doc), 200
 
 @app.route('/photos', methods=['POST'])
 def create():
 
-    item_doc = {
+    doc = {
         'uuid': uuid.uuid4().hex,
         'name': request.form['name'],
         'description': request.form['description'],
         'base64': request.form['base64']
     }
-    db.photos.insert_one(item_doc)
-    item_doc.pop('_id')
+    db.photos.insert_one(doc)
+    doc.pop('_id')
+    doc['url'] = f"{request.host}/img/{doc['uuid']}"
+
+    imgdata = base64.b64decode(doc['base64'].split(",")[1])
+    filename = doc['uuid'] + '.jpeg'
+    with open( './tmp/' + filename, 'wb') as f:
+        f.write(imgdata)
 
     # base64 to img
 
     # store img to S3
 
-    return jsonify(item_doc), 201
+    return jsonify(doc), 201
+
+@app.route('/photos/<photo_id>', methods=['DELETE'])
+def delete(photo_id):
+    doc = db.photos.find_one_and_delete({"uuid":photo_id})
+    if doc is None:
+        return jsonify({}), 404
+    else:
+        doc.pop('_id')
+        return jsonify(doc), 200
+
 
 @app.route('/img/<photo_id>', methods=['GET'])
 def img(photo_id):
+
+    doc = db.photos.find_one({"uuid":photo_id})
+    return send_from_directory( 
+        './tmp',
+        filename=doc['uuid'] + '.jpeg', 
+        mimetype="image/jpeg")
+
+
     # get from S3
 
     # return send_file( doc['base64'].split(',')[1], mimetype="image/jpeg")
-    # return send_file( s3file, mimetype="image/jpeg")
-    return "TODO"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
